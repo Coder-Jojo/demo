8620b6e6e0288afafc17cf9e8b2b95a64c6c1c1c
nitink4472 <55016971+nitink4472@users.noreply.github.com>
2020-10-16 13:19:19 +0530

    Update q4.cpp

--- q4.cpp
+++ q4.cpp
@@ -56,6 +56,115 @@ node* newNode (int data)
     return Node;  
 }  
 
+void zigzagtraversal(node* root) 
+{  
+  if (root==NULL) 
+    return; 
+ 
+  stack<node*> currentlevel; 
+  stack<node*> nextlevel; 
+
+  currentlevel.push(root); 
+
+  bool lefttoright = true; 
+  while (!currentlevel.empty()) { 
+ 
+    node* temp = currentlevel.top(); 
+    currentlevel.pop(); 
+
+    if (temp) { 
+
+      cout << temp->data << " "; 
+
+      if (lefttoright) { 
+        if (temp->left) 
+          nextlevel.push(temp->left); 
+        if (temp->right) 
+          nextlevel.push(temp->right); 
+      } 
+
+      else { 
+        if (temp->right) 
+          nextlevel.push(temp->right); 
+        if (temp->left) 
+          nextlevel.push(temp->left); 
+      } 
+    } 
+
+    if (currentlevel.empty()) { 
+      lefttoright = !lefttoright; 
+      swap(currentlevel, nextlevel); 
+    } 
+  } 
+} 
+
+int HeightOfTree(node* root) 
+{ 
+    if (root == NULL) 
+	{  return 0;} 
+  
+    int lheight = HeightOfTree(root->left); 
+    int rheight = HeightOfTree(root->right); 
+  
+    return max(lheight + 1, rheight + 1); 
+} 
+  
+void Print_Level_Right_To_Left(node* root, int level) 
+{ 
+    if (root == NULL) 
+	{return; }
+  
+    if (level == 1) 
+	{ cout << root->data << " "; }
+  
+    else if (level > 1) { 
+        Print_Level_Right_To_Left(root->right, level - 1); 
+        Print_Level_Right_To_Left(root->left, level - 1); 
+    } 
+} 
+  
+void Print_Level_Left_To_Right(node* root, int level) 
+{ 
+    if (root == NULL) 
+	{return;} 
+  
+    if (level == 1) 
+	{cout << root->data << " "; }
+  
+    else if (level > 1) { 
+        Print_Level_Left_To_Right(root->left, level - 1); 
+        Print_Level_Left_To_Right(root->right, level - 1); 
+    } 
+} 
+  
+void PrintReverseZigZag(node* root) 
+{ 
+    int flag = 1; 
+  
+    int height = HeightOfTree(root); 
+  
+    for (int i = height; i >= 1; i--) { 
+        if (flag == 1) { 
+            Print_Level_Right_To_Left(root, i); 
+            flag = 0; 
+        } 
+        else if (flag == 0) { 
+            Print_Level_Left_To_Right(root, i); 
+            flag = 1; 
+        } 
+    } 
+}
+void printPostorder(node* root) 
+{ 
+    if (root == NULL) 
+	{return;} 
+ 
+    printPostorder(root->left); 
+ 
+    printPostorder(root->right); 
+  
+    cout << root->data << " "; 
+}
 int main()
 {
 	vector<int> vec;
@@ -76,6 +185,18 @@ indata >> num;
       indata >> num; 
    }
    indata.close();
+   
    node *root = buildTree(vec, 0, vec.size() - 1);  
+   
+   cout<<"The post-order traversal:"<<endl;
+   printPostorder(root);
+   cout<<endl;
+   cout<<"The zig-zag traversal of the binary tree is: "<<endl;
+   zigzagtraversal(root);
+   cout<<endl;
+   cout<<"The Reverse zig-zag traversal is:"<<endl;
+   PrintReverseZigZag(root);
+   
+    
    return 0;
 }
